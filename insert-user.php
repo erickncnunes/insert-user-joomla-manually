02c16a656c730a6120982fabbfe0ab8336d843a5
Erick Nunes <erick.nunes.carvalho@gmail.com>
2014-12-19 22:46:51 -0200

    Correção script joomla 3.3.6
    Apenas efetuei a correção de alguns dados e o caminho jPATH

--- insert-user.php
+++ insert-user.php
@@ -1,21 +1,21 @@
 <?php
 define( '_JEXEC', 1 );
-define('JPATH_BASE', dirname(__FILE__) );//Caso o script fique na raiz do site, não precisa mecher em nada
+define('JPATH_BASE', dirname(__FILE__) . '../../../../../' ); //Caso utilize na raiz do site retirar . '../../../../../'
 define( 'DS', DIRECTORY_SEPARATOR );
 require_once ( JPATH_BASE .DS.'includes'.DS.'defines.php' );
 require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' );
-$mainframe =& JFactory::getApplication('site');
+$mainframe = JFactory::getApplication('site');
 $mainframe->initialise();
 jimport('joomla.user.helper');
 
 	//Dados do usuário que será Inserido manualmente no joomla
-	$nome = 'Jose da Silva Testador';
+	$nome = 'Jose Testador da Silva';
 	$login = 'teste';
 	$email    = 'teste@teste.com.br';
 	$pass = 'teste';       
 	
-//variavel que passa os dados para o joomla
-//em groups (11) é o id do grupo do joomla que o usuario será cadastrado, mude conforme sua necessidade
+//Variavel que passa os dados para o joomla
+//Em groups (11) é o id do grupo do joomla que o usuario será cadastrado, mude conforme sua necessidade
     $udata = array(
         "name"=>$nome,
         "username"=>$login,
@@ -28,14 +28,32 @@ jimport('joomla.user.helper');
 
     $user = new JUser;
 
-    //Write to database
-    if(!$user->bind($udata)) {
-      throw new Exception("Could not bind data. Error: " . $user->getError());
-    }
-    if (!$user->save()) {
-      throw new Exception("Could not save user. Error: " . $user->getError());
-    }
+    try
+	{
+		//Write to database
+		if(!$user->bind($udata)) {
+		  $success = 'false';
+	      $msg = 'Could not bind data. Error: ' . $user->getError();
+		}
+		
+		if (!$user->save()) {
+		  $success = 'false';
+		  $msg = 'Could not save user. Error: ' . $user->getError();
+		}
+	}
+	catch (RuntimeException $e)
+	{
+		$success = 'false';
+		$msg = 'Erro desconhecido: ' . $e.getMessage();
+	}
 
-    $new_user_id = $user->id;
-
-    echo $output;
+	if ($success == 'true')
+	{
+		$new_user_id = $user->id;
+		$success = 'true';
+		$msg = '';
+	}
+	//Pegando retorno REST na integração com um sistema
+	//echo '<retorno><success>' . $success . '</success><msg>' . $msg . '</msg></retorno>';
+}
+?>
